26e1815863de93c583db05e6dc4f6201def2e5f7
Anjo Pagdanganan <anjopag31@berkeley.edu>
2022-10-16 10:29:00 -0700

    added selenium testing thing

--- backend/selenium_test.py
+++ backend/selenium_test.py
@@ -1,1 +1,13 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 url = "https://www.hulu.com/watch/e3e44e69-6b56-44e9-b083-cbcb7c9730a5"
+xpath = ""
+
+def familyGuyTime():
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    driver.get(url)
+    driver.find_element_by_xpath(xpath)
+    
+
