f5845349bd63986c477c8984a28034fe07bd8e44
sueyoungshim <sueyoungshim@berkeley.edu>
2022-10-15 23:30:53 -0700

    deleted react image from app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import TextareaValidator from './comment.js'
 
@@ -6,19 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Comments 2 Comments
           <TextareaValidator></TextareaValidator>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
